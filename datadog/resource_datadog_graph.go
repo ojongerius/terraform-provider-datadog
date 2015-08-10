787c94978edf17fd032aa762498a0ad8e745feb1
Otto Jongerius <ojongerius@atlassian.com>
2015-08-10 18:41:58 +1000

    Prepare improvements by adding and updating comments.

--- datadog/resource_datadog_graph.go
+++ datadog/resource_datadog_graph.go
@@ -27,7 +27,6 @@ func resourceDatadogGraph() *schema.Resource {
 		Read:   resourceDatadogGraphRead,
 		Delete: resourceDatadogGraphDelete,
 		Update: resourceDatadogGraphUpdate,
-		//TODO: add Update
 
 		Schema: map[string]*schema.Schema{
 			"dashboard_id": &schema.Schema{
@@ -38,7 +37,6 @@ func resourceDatadogGraph() *schema.Resource {
 			},
 			"description": &schema.Schema{
 				Type:     schema.TypeString,
-				//Computed: true, // TODO: what does this do?
 				ForceNew: true,
 				Optional: true,
 			},
@@ -82,7 +80,7 @@ func resourceDatadogGraphCreate(d *schema.ResourceData, meta interface{}) error 
 	// it's a virtual resource, a la "resource_vpn_connection_route"
 	// hence we will need to do a bit of hacking to find out what dashboard.
 
-	// TODO: Delete the default graph with title "Mandatory default graph title"
+	// TODO: Delete placeholder graph. See https://github.com/ojongerius/terraform-provider-datadog/issues/8
 
 	resourceDatadogGraphUpdate(d, meta)
 
@@ -104,9 +102,7 @@ func resourceDatadogGraphCreate(d *schema.ResourceData, meta interface{}) error 
 func resourceDatadogGraphExists(d *schema.ResourceData, meta interface{}) (bool, error) {
 	client := meta.(*datadog.Client)
 
-	// TODO:
-	// * Does the dashboard exist? TODO: create a helper function for this one
-
+	// Verify our Dashboard(s) exist
 	_, err := client.GetDashboard(d.Get("dashboard_id").(int))
 
 	if err != nil {
@@ -117,9 +113,7 @@ func resourceDatadogGraphExists(d *schema.ResourceData, meta interface{}) (bool,
 		return false, fmt.Errorf("Error retrieving dashboard: %s", err)
 	}
 
-
-	// * Does the graph exist?
-
+	// Verify we exist
 	err = resourceDatadogGraphRead(d, meta)
 
 	if err != nil {
@@ -140,19 +134,18 @@ func resourceDatadogGraphRead(d *schema.ResourceData, meta interface{}) error {
 }
 
 func resourceDatadogGraphRetrieve(d *schema.ResourceData, meta interface{}) error {
-	// Here we go, we'll need to go into the dash and find ourselves
 	client := meta.(*datadog.Client)
 
+	// Get the dashboard(s)
 	dashboard, err := client.GetDashboard(d.Get("dashboard_id").(int))
 
 	if err != nil {
 		return fmt.Errorf("Error retrieving associated dashboard: %s", err)
 	}
 
+	// Walk through the graphs
 	for _, g := range dashboard.Graphs {
-		// TODO: efficiently test if the are the same. There are no ID, and there might be changes.
-		// for now we'll use the title for as unique identifier. Interested in different strategies..
-
+		// TODO: Using the title as unique identifier is 'suboptimal'. Interested in different strategies.
 		if g.Title == d.Get("title") {
 			fmt.Println("Found matching title. Start setting/saving state.")
 			d.Set("dashboard_id", d.Get("dashboard_id"))
@@ -182,7 +175,7 @@ func resourceDatadogGraphRetrieve(d *schema.ResourceData, meta interface{}) erro
 		}
 	}
 
-	// If we are still around we've not found ourselves, set SetId to empty so Terraform will create us.
+	// If we are still around we've not found ourselves. Set SetId to empty so Terraform will create the resource for us.
 	d.SetId("")
 
 	return nil
@@ -191,6 +184,7 @@ func resourceDatadogGraphRetrieve(d *schema.ResourceData, meta interface{}) erro
 func resourceDatadogGraphUpdate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*datadog.Client)
 
+	// Get the dashboard
 	dashboard, err := client.GetDashboard(d.Get("dashboard_id").(int))
 
 	if err != nil {
@@ -201,11 +195,11 @@ func resourceDatadogGraphUpdate(d *schema.ResourceData, meta interface{}) error 
 
 	log.Printf("[DEBUG] Checking if requests have changed.")
 
+	// Check if there are changes
 	if d.HasChange("request") {
+
 		graph_definition := datadog.Graph{}.Definition
-
 		graph_requests := datadog.Graph{}.Definition.Requests
-
 		graph_definition.Viz = d.Get("viz").(string)
 
 		log.Printf("[DEBUG] Request has changed.")
@@ -213,7 +207,7 @@ func resourceDatadogGraphUpdate(d *schema.ResourceData, meta interface{}) error 
 		ors := o.(*schema.Set).Difference(n.(*schema.Set))
 		nrs := n.(*schema.Set).Difference(o.(*schema.Set))
 
-		// Now first loop through all the old routes and delete any obsolete ones
+		// Loop through all the old requests and delete any obsolete ones
 		for _, request := range ors.List() {
 			m := request.(map[string]interface{})
 
@@ -223,6 +217,7 @@ func resourceDatadogGraphUpdate(d *schema.ResourceData, meta interface{}) error 
 			log.Printf("[DEBUG] Deleting graph stacked %t", m["stacked"].(bool))
 
 		}
+		// Loop through all the new requests and append them
 		for _, request := range nrs.List() {
 			m := request.(map[string]interface{})
 
@@ -234,9 +229,8 @@ func resourceDatadogGraphUpdate(d *schema.ResourceData, meta interface{}) error 
 		}
 
 
-		// TODO: should this not only by done when there is a change?
+		// Add requests to the graph definition
 		graph_definition.Requests = graph_requests
-
 		the_graph := datadog.Graph{Title: d.Get("title").(string), Definition: graph_definition}
 
 		dashboard.Graphs = append(dashboard.Graphs, the_graph) // Should be done for each
@@ -254,30 +248,29 @@ func resourceDatadogGraphUpdate(d *schema.ResourceData, meta interface{}) error 
 	}
 
 	return nil
+
+	// TODO: still need this?
 	//return resourceDatadogGraphRead(d, meta)
 }
 
 func resourceDatadogGraphDelete(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*datadog.Client)
 
-	// Grab the dashboard and fetch it.
+	// Get the dashboard
 	dashboard, err := client.GetDashboard(d.Get("dashboard_id").(int))
 
 	if err != nil {
 		return fmt.Errorf("Error retrieving associated dashboard: %s", err)
 	}
 
-	// Now we will construct a new version of the graphs and call update with all the graphs
-	// apart from the current one.
+	// Build a new slice of graphs, without the nominee to deleted.
 	// TODO: Use the set for this.
 	new_graphs := []datadog.Graph{}
 	for _, r := range dashboard.Graphs {
-		// TODO: efficiently test if the are the same
-		// for this POC we'll just match on title
+		// TODO: efficiently test if the are the same for this POC we'll just match on title
 		if r.Title == d.Get("title") {
 			continue
 		} else {
-			// Keep this graph
 			new_graphs = append(new_graphs, r)
 		}
 	}
