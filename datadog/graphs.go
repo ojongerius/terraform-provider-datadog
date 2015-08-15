851e92064160dd0c9a1fdb274357fa01ac363348
Otto Jongerius <ojongerius@atlassian.com>
2015-08-15 12:07:34 +1000

    Thanks Intellij.

--- datadog/graphs.go
+++ datadog/graphs.go
@@ -5,12 +5,12 @@ import (
 )
 
 func createPlaceholderGraph() []datadog.Graph {
-    // Return a dummy placeholder graph.
-    // This should be used when creating new dashboards, or removing the last
-    // in a board.
-    // Background; An API call to create or update dashboards (Timeboards) will
-    // fail if it contains zero graphs. This is a bug in the Datadog API,
-    // as dashboards *can* exist without any graphs.
+	// Return a dummy placeholder graph.
+	// This should be used when creating new dashboards, or removing the last
+	// in a board.
+	// Background; An API call to create or update dashboards (Timeboards) will
+	// fail if it contains zero graphs. This is a bug in the Datadog API,
+	// as dashboards *can* exist without any graphs.
 
 	graph_definition := datadog.Graph{}.Definition
 	graph_definition.Viz = "timeseries"
